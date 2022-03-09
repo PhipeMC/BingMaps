e1dcd430e33ba3bb561d830a0561864895a528cd
PhipeMC <phipe.mcbukkit@gmail.com>
2022-03-09 16:42:29 -0600

    Ruta agregada

--- app/src/main/java/net/phipe/bingmaps/MainActivity.java
+++ app/src/main/java/net/phipe/bingmaps/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.widget.FrameLayout;
+
+import com.microsoft.maps.Geopath;
+import com.microsoft.maps.Geoposition;
+import com.microsoft.maps.MapPolygon;
+import com.microsoft.maps.MapPolyline;
 import com.microsoft.maps.MapRenderMode;
 import com.microsoft.maps.MapView;
 import com.microsoft.maps.Geopoint;
@@ -18,6 +24,9 @@ import com.microsoft.maps.MapImage;
 import com.microsoft.maps.MapStyleSheets;
 import com.microsoft.maps.MapProjection;
 import com.microsoft.maps.MapRouteLine;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,19 +49,30 @@ public class MainActivity extends AppCompatActivity {
         mPinLayer = new MapElementLayer();
         mMapView.getLayers().add(mPinLayer);
 
-        Geopoint location = new Geopoint(20.140062, -101.150552);  // your pin lat-long coordinates
-        String title = "Pin";       // title to be shown next to the pin
-        //Bitmap pinBitmap = ...   // your pin graphic (optional)
 
-        MapIcon pushpin = new MapIcon();
-        pushpin.setLocation(location);
-        pushpin.setTitle(title);
-        //pushpin.setImage(new MapImage(pinBitmap));
-
-        mPinLayer.getElements().add(pushpin);
         mMapView.setMapStyleSheet(MapStyleSheets.roadDark());
         mMapView.setMapProjection(MapProjection.GLOBE);
         mMapView.setMapProjection(MapProjection.WEB_MERCATOR);
+
+        drawLineOnMap(mMapView);
+    }
+
+    void drawLineOnMap(MapView mapView) {
+        Geoposition center = mapView.getCenter().getPosition();
+
+        ArrayList<Geoposition> geopoints = fillData();
+
+        MapPolyline mapPolyline = new MapPolyline();
+        mapPolyline.setPath(new Geopath(geopoints));
+        mapPolyline.setStrokeColor(Color.rgb(0,164,239));
+        mapPolyline.setStrokeWidth(5);
+        mapPolyline.setStrokeDashed(false);
+
+        // Add Polyline to a layer on the map control.
+        MapElementLayer linesLayer = new MapElementLayer();
+        linesLayer.setZIndex(1.0f);
+        linesLayer.getElements().add(mapPolyline);
+        mapView.getLayers().add(linesLayer);
     }
 
     @Override
@@ -77,5 +97,54 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
         mMapView.onSaveInstanceState(outState);
+    }
+
+    public ArrayList<Geoposition> fillData() {
+        ArrayList<Geoposition> geopoints = new ArrayList<Geoposition>();
+        geopoints.add(new Geoposition(20.131912,
+                -101.181968));
+        geopoints.add(new Geoposition(20.132098, -101.182085));
+        geopoints.add(new Geoposition(20.135686,
+                -101.180554));
+        geopoints.add(new Geoposition(20.135493,
+                -101.178985));
+        geopoints.add(new Geoposition(20.136591,
+                -101.178816));
+        geopoints.add(new Geoposition(20.136665,
+                -101.176773));
+        geopoints.add(new Geoposition(20.1396,
+                -101.176733));
+        geopoints.add(new Geoposition(20.139332,
+                -101.171185));
+        geopoints.add(new Geoposition(20.141595,
+                -101.171048));
+        geopoints.add(new Geoposition(20.142849,
+                -101.157803));
+        geopoints.add(new Geoposition(20.14293,
+                -101.156401));
+        geopoints.add(new Geoposition(20.1435,
+                -101.149911));
+        geopoints.add(new Geoposition(20.140523,
+                -101.150548));
+        geopoints.add(new Geoposition(20.140523,
+                -101.150548));
+        return geopoints;
+    }
+
+    public void pointsMap(){
+        ArrayList<Geoposition> list = fillData();
+        for (Geoposition gp: list){
+            mPinLayer.getElements().add(new MapIcon());
+        }
+        Geopoint location = new Geopoint(20.140062, -101.150552);  // your pin lat-long coordinates
+        String title = "Pin";       // title to be shown next to the pin
+        //Bitmap pinBitmap = ...   // your pin graphic (optional)
+
+        MapIcon pushpin = new MapIcon();
+        pushpin.setLocation(location);
+        pushpin.setTitle(title);
+        //pushpin.setImage(new MapImage(pinBitmap));
+
+        mPinLayer.getElements().add(pushpin);
     }
 }
