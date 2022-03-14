274a42d9e1fdfc171ab3d5bffb519f5288bf82cd
PhipeMC <phipe.mcbukkit@gmail.com>
2022-03-14 16:55:23 -0600

    Pin y posicion agregados

--- app/src/main/java/net/phipe/bingmaps/MainActivity.java
+++ app/src/main/java/net/phipe/bingmaps/MainActivity.java
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
+import com.microsoft.maps.GPSMapLocationProvider;
 import com.microsoft.maps.Geopath;
 import com.microsoft.maps.Geoposition;
+import com.microsoft.maps.MapElement;
+import com.microsoft.maps.MapFlyout;
 import com.microsoft.maps.MapPolygon;
 import com.microsoft.maps.MapPolyline;
 import com.microsoft.maps.MapRenderMode;
+import com.microsoft.maps.MapTappedEventArgs;
+import com.microsoft.maps.MapUserLocation;
+import com.microsoft.maps.MapUserLocationTrackingState;
 import com.microsoft.maps.MapView;
 import com.microsoft.maps.Geopoint;
 import com.microsoft.maps.MapAnimationKind;
@@ -24,15 +34,20 @@ import com.microsoft.maps.MapImage;
 import com.microsoft.maps.MapStyleSheets;
 import com.microsoft.maps.MapProjection;
 import com.microsoft.maps.MapRouteLine;
+import com.microsoft.maps.OnMapTappedListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
     private static final Geopoint ITSUR = new Geopoint(20.140062, -101.150552);
     private MapElementLayer mPinLayer;
+    private boolean locationPin = false;
+    private int indexLocationPin = 0;
+    private Button btn_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +65,54 @@ public class MainActivity extends AppCompatActivity {
         mMapView.getLayers().add(mPinLayer);
 
 
-        mMapView.setMapStyleSheet(MapStyleSheets.roadDark());
+        mMapView.setMapStyleSheet(MapStyleSheets.roadLight());
         mMapView.setMapProjection(MapProjection.GLOBE);
         mMapView.setMapProjection(MapProjection.WEB_MERCATOR);
 
+        mMapView.addOnMapTappedListener(new OnMapTappedListener() {
+            @Override
+            public boolean onMapTapped(MapTappedEventArgs mapTappedEventArgs) {
+                Geopoint position = mapTappedEventArgs.location;
+
+                if(!locationPin) {
+                    String pin = "Destino";
+                    MapIcon pintest = new MapIcon();
+                    pintest.setLocation(position);
+                    pintest.setTitle(pin);
+                    indexLocationPin = mPinLayer.getElements().size()-1;
+                    Log.d("GPS","index: "+Integer.toString(indexLocationPin));
+                    mPinLayer.getElements().add(indexLocationPin,pintest);
+                    locationPin = true;
+                }
+
+                return false;
+            }
+        });
+
+        MapUserLocation userLocation = mMapView.getUserLocation();
+
+        MapUserLocationTrackingState userLocationTrackingState = userLocation.startTracking(new GPSMapLocationProvider.Builder(getApplicationContext()).build());
+        if (userLocationTrackingState == MapUserLocationTrackingState.PERMISSION_DENIED)
+        {
+            // request for user location permissions and then call startTracking again
+        } else if (userLocationTrackingState == MapUserLocationTrackingState.READY)
+        {
+            // handle the case where location tracking was successfully started
+        } else if (userLocationTrackingState == MapUserLocationTrackingState.DISABLED)
+        {
+            // handle the case where all location providers were disabled
+        }
+
         drawLineOnMap(mMapView);
         drawPins();
+
+        btn_delete = findViewById(R.id.btn_delete);
+        btn_delete.setOnClickListener(view -> {
+            if(locationPin){
+                locationPin = false;
+                mPinLayer.getElements().remove(indexLocationPin);
+            }
+        });
     }
 
     void drawLineOnMap(MapView mapView) {
@@ -104,7 +161,8 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Geoposition> geopoints = new ArrayList<Geoposition>();
         geopoints.add(new Geoposition(20.131912,
                 -101.181968));
-        geopoints.add(new Geoposition(20.132098, -101.182085));
+        geopoints.add(new Geoposition(20.132098,
+                -101.182085));
         geopoints.add(new Geoposition(20.135686,
                 -101.180554));
         geopoints.add(new Geoposition(20.135493,
@@ -133,17 +191,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void drawPins(){
-        Geopoint location = new Geopoint(20.131993, -101.181831);  // your pin lat-long coordinates
         Geopoint l1 = new Geopoint(20.139561, -101.150646);
         String pin = "ITSUR";
-        String title = "Casa";
         MapIcon pin1 = new MapIcon();
-        MapIcon pushpin = new MapIcon();
         pin1.setLocation(l1);
-        pushpin.setLocation(location);
         pin1.setTitle(pin);
-        pushpin.setTitle(title);
         mPinLayer.getElements().add(pin1);
-        mPinLayer.getElements().add(pushpin);
+        MapFlyout flyout = new MapFlyout();
+        flyout.setTitle("ITSUR");
+        flyout.setDescription("Universidad de GTO");
+        pin1.setFlyout(flyout);
     }
 }
